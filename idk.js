075db73e71765284584658b7db1d47e6a650391b
Alexander Køpke <alexanderkopke@outlook.com>
2020-05-09 09:41:32 +0200

    stuff

--- idk.js
+++ idk.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
-axios.get('https://github.com/alexanderkopke/philobuddy/blob/master/quotes.json')
-    .then(function (response) {
-        console.log('hello')
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    //https://github.com/alexanderkopke/philobuddy/blob/master/quotes.json
+
+
+axios.get('https://github.com/alexanderkopke/philobuddy/blob/master/quotes.json', function (res) {
+    console.log(res);
+
+});
+
+//https://github.com/alexanderkopke/philobuddy/blob/master/quotes.json
